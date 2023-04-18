cc9fc88a5d28eee3d8b4d4267188019bb4bf0f6f
ebmaj7 <ebmaj7@proton.me>
2023-04-18 09:13:11 +0200

    add hacking header to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/** 
+ * This file is part of Minos
+ *
+ * Copyright (C) 2023 ebmaj7 <ebmaj7@proton.me>
+ *
+ * Minos is a hack. You can use it according to the terms and
+ * conditions of the Hacking License (see licenses/HACK.txt)
+ */ 
+
 const { session, BrowserWindow, app } = require('electron');
 const { appendFile } = require('fs');
 
