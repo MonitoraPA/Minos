627f9f1bc1801d9a7a083af4ec415ed041d149ea
ebmaj7 <ebmaj7@proton.me>
2023-05-24 17:39:37 +0200

    remove useless Page function for now

--- src/cdp2har.js
+++ src/cdp2har.js
@@ -11,14 +11,3 @@
  * Portions of this file are Copyright (C) 2020 Andrea Cardaci <cyrus.and@gmail.com>
  * (see licenses/MIT.txt)
  */
-
-function Page(url){
-	this._options = options;
-	this._responseBodyCounter = 0;
-	this.url = url;
-	this.firstRequestId = undefined;
-	this.firstRequestMs = undefined;
-	this.domContentEventFiredMs = undefined;
-	this.loadEventFiredMs = undefined;
-	this.entries = new Map();
-}
