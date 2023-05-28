8c15c855d39204cbadf396617d12e41a27c05f6c
ebmaj7 <ebmaj7@proton.me>
2023-05-28 18:10:52 +0200

    add copyright statement to page.js

--- src/page.js
+++ src/page.js
@@ -1,3 +1,17 @@
+/** 
+ * This file is part of Minos
+ *
+ * Copyright (C) 2023 ebmaj7 <ebmaj7@proton.me>
+ *
+ * Minos is a hack. You can use it according to the terms and
+ * conditions of the Hacking License (see licenses/HACK.txt)
+ */ 
+
+/**
+ * Portions of this file are Copyright (C) 2020 Andrea Cardaci <cyrus.and@gmail.com>
+ * (see licenses/MIT.txt)
+ */
+
 function Page(url){
 	this._responseBodyCounter = 0;
 	this.url = url;
