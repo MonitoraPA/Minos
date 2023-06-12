1f07d9d03a8841e19f31ff251218712425de3696
ebmaj7 <ebmaj7@proton.me>
2023-06-12 17:24:16 +0200

    add form validation

--- src/renderer.js
+++ src/renderer.js
@@ -45,7 +45,6 @@ const setupTooltips = () => {
 			const tooltip = document.createElement('div');
 			hideComponent(tooltip);
 			tooltip.classList.add('tooltip');
-			tooltip.innerText = ttext;
 			document.getElementsByTagName('body')[0].appendChild(tooltip);
 			// setup event listeners
 			button.addEventListener('mouseenter', (event) => {
@@ -53,6 +52,7 @@ const setupTooltips = () => {
 					const {x, y, width, height} = event.target.getBoundingClientRect();
 					tooltip.style.left = `${x - 30}px`;
 					tooltip.style.top = `${y + height + 5}px`;
+					tooltip.innerText = button.getAttribute('tooltip');
 					showComponent(tooltip);
 				}
 				else 
@@ -67,7 +67,8 @@ const setupTooltips = () => {
 
 const onDOMContentLoaded = () => {
 	setupTooltips();
-	const [verifyButton, topButton, urlBox, textarea, reportButton, reportLabel] = getElementsByIds(['verify-button', 'top-button', 'url-box', 'report', 'report-button', 'report-label']);
+	const [verifyButton, topButton, urlBox, textarea, reportButton, reportLabel, nextButton, prevButton] = getElementsByIds(['verify-button', 'top-button', 'url-box', 'report', 'report-button', 'report-label',  'button-next', 'button-prev']);
+	const [formName, formSurname, formBirthdate, formBirthplace, formFisccode, formAddress] = getElementsByIds(['form-name', 'form-surname', 'form-birthdate', 'form-birthplace', 'form-fisccode', 'form-address']);
 	// const verifyButton = document.getElementById('verify-button');
 	verifyButton.addEventListener('click', () => {
 		hideComponent(document.getElementById('main'));
@@ -95,6 +96,71 @@ const onDOMContentLoaded = () => {
 		}
 	};
 	topButton.addEventListener('click', onClickStart); 
+
+	const validateForm = () => {
+		const showTooltip = (err) => { nextButton.classList.add('disabled'); nextButton.setAttribute('tooltip', err); }
+		const hideTooltip = () => { nextButton.classList.remove('disabled'); }
+		// first remove invalid from all form elements
+		[formName, formSurname, formBirthdate, formBirthplace, formFisccode, formAddress].forEach(target => target.classList.remove('invalid'));
+		// then possibly re-add invalid and display error tooltip above next button
+		let err = "";
+		if(formAddress.value.length === 0){
+			formAddress.classList.add('invalid');
+			err = "Riempi tutti i campi prima di procedere";
+		}
+		if(!formFisccode.value.match(/[A-Z]{3}[A-Z]{3}[0-9]{2}[ABCDEHLMPRST]((0[1-9]|[1-2][0-9]|3[0-1])|([4-6][1-9]|7[0-1]))[A-Z][0-9]{3}[A-Z]/g)){
+			formFisccode.classList.add('invalid');
+			err = "Inserisci un codice fiscale valido";
+		}
+		if(formBirthplace.value.length === 0){
+			formBirthplace.classList.add('invalid');
+			err = "Riempi tutti i campi prima di procedere";
+		}
+		if(!formBirthdate.value.match(/((0[1-9]|1[0-9]|2[0-9]|30)\/(04|06|09|11)\/([1-2][0-9]{3}))|((0[1-9]|1[0-9]|2[0-9])\/02\/([1-2][0-9]{3}))|((0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(01|03|05|07|08|10|12)\/([1-2][0-9]{3}))/g)){
+			formBirthdate.classList.add('invalid');
+			err = "Inserisci una data di nascita valida";
+		}
+		if(formSurname.value.length === 0){
+			formSurname.classList.add('invalid');
+			err = "Riempi tutti i campi prima di procedere";
+		}
+		if(formName.value.length === 0){
+			formName.classList.add('invalid');
+			err = "Riempi tutti i campi prima di procedere";
+		}
+		if(err.length === 0)
+			hideTooltip();
+		else 
+			showTooltip(err);
+	};
+
+	formName.addEventListener('blur', validateForm);
+	formSurname.addEventListener('blur', validateForm);
+	formFisccode.addEventListener('blur', validateForm);
+	formBirthplace.addEventListener('blur', validateForm);
+	formAddress.addEventListener('blur', validateForm);
+	formBirthdate.addEventListener('blur', validateForm);
+	formBirthdate.addEventListener('keydown', (event) => {
+		event.preventDefault();
+		const current = event.target.value;
+		if(event.keyCode === 8){
+			if([0,1,4,7,8,9,10].some(x => x === current.length))
+				event.target.value = current.slice(0, -1);
+			else if([3,6].some(x => x === current.length))
+				event.target.value = current.slice(0, -2);
+		}
+		if([0,1,2,3,4,5,6,7,8,9].some(x => x == event.key) && current.length < 10){
+			// simply insert letter
+			if([0,3,6,7,8,9].some(x => x === current.length))
+				event.target.value = current + event.key;
+			// insert letter and slash
+			else if([1,4].some(x => x === current.length))
+				event.target.value = current + event.key + "/";
+		}
+	});
+
+	nextButton.addEventListener('mouseenter', validateForm);
+
 	// on page navigation, update the URL in the urlBox
 	window.electronAPI.onChangeURL((event, url) => {
 		urlBox.value = url;
