4906f53269b9e18d64c399d57f21a38c7d95543f
ebmaj7 <ebmaj7@proton.me>
2023-06-15 13:26:04 +0200

    split error from validation, simplify listeners

--- src/renderer.js
+++ src/renderer.js
@@ -119,7 +119,7 @@ const onDOMContentLoaded = () => {
 				// first remove invalid from all form elements
 				[formName, formSurname, formBirthdate, formBirthplace, formFisccode, formAddress].forEach(target => target.classList.remove('invalid'));
 				// then possibly re-add invalid and display error tooltip above next button
-				let err = "";
+				let err = undefined;
 				if(formAddress.value.length === 0){
 					formAddress.classList.add('invalid');
 					err = "Riempi tutti i campi";
@@ -144,16 +144,7 @@ const onDOMContentLoaded = () => {
 					formName.classList.add('invalid');
 					err = "Riempi tutti i campi";
 				}
-				if(err.length === 0){
-					pageValid[0] = true;
-					enable(nextButton);
-				}
-				else {
-					pageValid[0] = false;
-					disable(nextButton);
-					nextButton.setAttribute('tooltip', err);
-				}
-				break;
+				return err;
 			case 1:
 				break;
 			case 2:
@@ -176,46 +167,79 @@ const onDOMContentLoaded = () => {
 		});
 	});
 
-	formName.addEventListener('blur', validateForm);
-	formSurname.addEventListener('blur', validateForm);
-	formFisccode.addEventListener('blur', validateForm);
-	formBirthplace.addEventListener('blur', validateForm);
-	formAddress.addEventListener('blur', validateForm);
-	formBirthdate.addEventListener('blur', validateForm);
+	const formHandler = () => {
+		const err = validateForm();
+		if(err){
+			pageValid[page] = false;
+			disable(nextButton);
+			nextButton.setAttribute('tooltip', err);
+		} else {
+			pageValid[page] = true;
+			enable(nextButton);
+		}
+	};
+
+	formName.addEventListener('input', formHandler);
+	formSurname.addEventListener('input', formHandler);
+	formFisccode.addEventListener('input', formHandler);
+	formBirthplace.addEventListener('input', formHandler);
+	formAddress.addEventListener('input', formHandler);
+	formBirthdate.addEventListener('input', formHandler);
 	formFisccode.addEventListener('keydown', (event) => {
 		// insert only uppercase letters
-		if(event.code !== "Tab" && event.code !== "Backspace"){
+		if(event.code !== "Tab" && event.code !== "Backspace" && event.code !== "ArrowLeft" && event.code !== "ArrowRight"){
 			if(event.target.value.length < 16 && (isLetter(event.key) || isDigit(event.key))){
 				event.preventDefault();
-				event.target.value = event.target.value + event.key.toUpperCase();
+				const curPos = event.target.selectionStart;
+				event.target.value = event.target.value.slice(0, curPos) + event.key.toUpperCase() + event.target.value.slice(curPos);
+				event.target.selectionStart = curPos + 1;
+				event.target.selectionEnd = curPos + 1;
 			} else {
 				event.preventDefault();
 			}
 		} 
-	});
-	formBirthdate.addEventListener('keydown', (event) => {
-		event.preventDefault();
-		const current = event.target.value;
-		if(event.code === "Backspace"){ // backspace
-			if([0,1,4,7,8,9,10].some(x => x === current.length))
-				event.target.value = current.slice(0, -1);
-			else if([3,6].some(x => x === current.length))
-				event.target.value = current.slice(0, -2);
-		}
-		else if(event.code === "Tab"){ // tab
-			formBirthplace.focus();
-		}
-		else if([0,1,2,3,4,5,6,7,8,9].some(x => x == event.key) && current.length < 10){
-			// simply insert letter
-			if([0,3,6,7,8,9].some(x => x === current.length))
-				event.target.value = current + event.key;
-			// insert letter and slash
-			else if([1,4].some(x => x === current.length))
-				event.target.value = current + event.key + "/";
-		}
+		formHandler();
 	});
 
-	nextButton.addEventListener('mouseenter', validateForm);
+	formBirthdate.addEventListener('keydown', (event) => {
+		const addSlashes = (date) => { 
+			if(date.length >= 4)
+				return date.slice(0, 2) + "/" + date.slice(2, 4) + "/" + date.slice(4);
+			else if(date.length >= 2)
+				return date.slice(0, 2) + "/" + date.slice(2);
+			else
+				return date;
+		};
+		const curText = event.target.value;
+		const textPos = event.target.selectionStart;
+		// remove slashes
+		const curDate = curText.replaceAll('/', '');
+		const datePos = textPos >= 6 ? textPos - 2 : (textPos >= 3 ? textPos - 1 : textPos);
+		let newDate = curDate;
+		// if there's a backspace delete backspace + number
+		if(event.code === "Backspace"){
+			event.preventDefault();
+			newDate = curDate.slice(0, datePos - 1) + curDate.slice(datePos);	
+			event.target.value = addSlashes(newDate);
+			const nextPos = [3,6].some(x => x === textPos) ? textPos - 2 : textPos - 1;
+			event.target.selectionStart = nextPos;
+			event.target.selectionEnd = nextPos;
+		} else if(event.code !== "Tab" && event.code !== "Backspace" && event.code !== "ArrowLeft" && event.code !== "ArrowRight"){
+			event.preventDefault();
+			// only allow numbers
+			if(Array.from(Array(10).keys()).map(x => String(x)).some(x => x === event.key) && curDate.length < 8){
+				// compute new date
+				newDate = curDate.slice(0, datePos) + event.key + curDate.slice(datePos);
+				event.target.value = addSlashes(newDate);
+				const nextPos = [1,2,4,5].some(x => x === textPos) ? textPos + 2 : textPos + 1;
+				event.target.selectionStart = nextPos;
+				event.target.selectionEnd = nextPos;
+			}
+		}
+		formHandler();
+	});
+
+	nextButton.addEventListener('mouseenter', formHandler);
 
 	nextButton.addEventListener('click', (event) => {
 		if(event.target.classList.contains('disabled'))
