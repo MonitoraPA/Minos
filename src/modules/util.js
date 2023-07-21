fc71ceaa717dcb5377a0d63c13d12be964d7c44f
ebmaj7 <ebmaj7@proton.me>
2023-07-21 10:12:03 +0200

    fix missing parentheses

--- src/modules/util.js
+++ src/modules/util.js
@@ -26,7 +26,7 @@ export const disable = (component) => { component.classList.add('disabled'); }
 
 const isUpper = (key) => { return key.charCodeAt(0) >= 'A'.charCodeAt(0) && key.charCodeAt(0) <= 'Z'.charCodeAt(0) };
 const isLower = (key) => { return key.charCodeAt(0) >= 'a'.charCodeAt(0) && key.charCodeAt(0) <= 'z'.charCodeAt(0) };
-export const isLetter = (key) => { return key.length === 1 && isUpper(key) || isLower(key) };
+export const isLetter = (key) => { return key.length === 1 && (isUpper(key) || isLower(key)) };
 export const isDigit = (key) => { return key.length === 1 && (key.charCodeAt(0) >= '0'.charCodeAt(0) && key.charCodeAt(0) <= '9'.charCodeAt(0)) };
 
 // convenience function to get more elements at once
